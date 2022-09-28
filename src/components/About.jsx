531de77c3a3cba984846952bb0266c1548c53281
Prashant Singh <psingh5025615@gmail.com>
2022-09-28 19:02:59 +0530

    card done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ import {FaBath} from 'react-icons/fa'
 const About = () => {
   return (
     <>
-        <div className=' flex  flex-col w-full h-[1240px] bg-gray-200'>
+        <div className='flex flex-col w-full h-[1240px] bg-gray-200'>
           <div className='mt-[100px] mb-8 '>
             <h6 className='font-mono text-lg font-bold text-yellow-500 text-center'>Queck Search</h6>
           </div>
@@ -14,25 +14,27 @@ const About = () => {
             <h1 className='text-4xl text-gray-800 font-sans font-bold my-2 font-mono'>San Francisco</h1>
           </div>
 
-          <div className='grid md:grid-cols-4 gap-4 px-2 items-center mt-12 text-gray-800'>
-            <div className='flex flex-col justify-center items-center border border-gray-300 py-8 rounded-xl shadow-xl  hover:text-white hover:bg-slate-900 '>
-              <FaBath className='text-blue-500' size={30}/>
-              <p className='font-bold font-mono text-xl mt-8'>Bathroom Remodel Contractors</p>
-            </div>
-            <div className='flex flex-col justify-center items-center border border-gray-300 py-8 rounded-xl shadow-xl  hover:text-white hover:bg-slate-900 '>
-              <MdOutlineShower className='text-pink-500 ' size={30}/>
-              <p className='font-bold font-mono text-xl mt-8'>Roofers</p>
-            </div>
-            <div className='flex flex-col justify-center items-center border border-gray-300 py-8 rounded-xl shadow-xl  hover:text-white hover:bg-slate-900 '>
-              <MdOutlineArchitecture className='text-green-500' size={30}/>
-              <p className='font-bold font-mono text-xl mt-8'>Remodeling Architects</p>
-            </div>
-            <div className='flex flex-col justify-center items-center border border-gray-300 py-8 rounded-xl shadow-xl hover:text-white hover:bg-slate-900 '>
-              <MdEngineering className='text-green-500' size={30}/>
-              <p className='font-bold font-mono text-xl mt-8'>Civil & Structural Engineers</p>
-            </div>
+          <div className='flex flex-wrap justify-evenly mt-8'>
+            <div className='flex flex-col sm:w-60 bg-white  rounded-lg shadow-md w-full m-6 justify-center items-center overflow-hidden hover:bg-slate-900 hover:text-white transition-all duration-500 '>
+              <FaBath className='text-blue-500 h-20 m-6 ' size={30}/>
+              <p className='text-center px-2 pb-5 font-mono font-bold'>Bathroom Remodel Contractors</p>
 
-            
+            </div>
+            <div className='flex flex-col sm:w-60 bg-white  rounded-lg shadow-md w-full m-6 justify-center items-center overflow-hidden hover:bg-slate-900 hover:text-white transition-all duration-500 '>
+              <FaBath className='text-blue-500 h-20 m-6 ' size={30}/>
+              <p className='text-center px-2 pb-5 font-mono font-bold'>Bathroom Remodel Contractors</p>
+
+            </div>
+            <div className='flex flex-col sm:w-60 bg-white  rounded-lg shadow-md w-full m-6 justify-center items-center overflow-hidden hover:bg-slate-900 hover:text-white transition-all duration-500 '>
+              <FaBath className='text-blue-500 h-20 m-6 ' size={30}/>
+              <p className='text-center px-2 pb-5 font-mono font-bold'>Bathroom Remodel Contractors</p>
+
+            </div>
+            <div className='flex flex-col sm:w-60 bg-white  rounded-lg shadow-md w-full m-6 justify-center items-center overflow-hidden hover:bg-slate-900 hover:text-white transition-all duration-500 '>
+              <FaBath className='text-blue-500 h-20 m-6 ' size={30}/>
+              <p className='text-center px-2 pb-5 font-mono font-bold'>Bathroom Remodel Contractors</p>
+
+            </div>
           </div>
 
 
@@ -45,7 +47,7 @@ const About = () => {
             <h1 className='text-4xl text-gray-800 font-sans font-bold my-2 font-mono'>San Francisco</h1>
           </div>
 
-          <div className='grid md:grid-cols-4 gap-4 px-2 items-center mt-12 text-gray-800'>
+          {/* <div className='grid md:grid-cols-4 gap-4 px-2 items-center mt-12 text-gray-800'>
             <div className='flex flex-col justify-center items-center border border-gray-300 py-8 rounded-xl shadow-xl  hover:text-white hover:bg-slate-900 '>
               <FaBath className='text-blue-500' size={30}/>
               <p className='font-bold font-mono text-xl mt-8'>Bathroom Remodel Contractors</p>
@@ -64,6 +66,24 @@ const About = () => {
             </div>
 
             
+          </div> */}
+
+          <div className='flex flex-wrap justify-evenly mt-8'>
+            <div className='flex flex-col sm:w-60 bg-white  rounded-lg shadow-md w-full m-6 justify-center items-center overflow-hidden hover:bg-slate-900 hover:text-white transition-all duration-500 '>
+              <FaBath className='text-blue-500 h-20 m-6 ' size={30}/>
+              <p className='text-center px-2 pb-5 font-mono font-bold'>Bathroom Remodel Contractors</p>
+
+            </div>
+            <div className='flex flex-col sm:w-60 bg-white  rounded-lg shadow-md w-full m-6 justify-center items-center overflow-hidden hover:bg-slate-900 hover:text-white transition-all duration-500 '>
+              <FaBath className='text-blue-500 h-20 m-6 ' size={30}/>
+              <p className='text-center px-2 pb-5 font-mono font-bold'>Bathroom Remodel Contractors</p>
+
+            </div>
+            <div className='flex flex-col sm:w-60 bg-white  rounded-lg shadow-md w-full m-6 justify-center items-center overflow-hidden hover:bg-slate-900 hover:text-white transition-all duration-500 '>
+              <FaBath className='text-blue-500 h-20 m-6 ' size={30}/>
+              <p className='text-center px-2 pb-5 font-mono font-bold'>Bathroom Remodel Contractors</p>
+
+            </div>
           </div>
           
 
