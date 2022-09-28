4411fa204d0a1c7f769d80ef785d9d1a4cee72b1
Prashant Singh <psingh5025615@gmail.com>
2022-09-28 15:42:26 +0530

    next changes are done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,74 @@
 import React from 'react'
+import {MdOutlineShower,MdEngineering,MdOutlineArchitecture} from 'react-icons/md'
+import {FaBath} from 'react-icons/fa'
 
 const About = () => {
   return (
-    <div>
-        <div></div>
-    </div>
+    <>
+        <div className=' flex  flex-col w-full h-[1240px] bg-gray-200'>
+          <div className='mt-[100px] mb-8 '>
+            <h6 className='font-mono text-lg font-bold text-yellow-500 text-center'>Queck Search</h6>
+          </div>
+          <div className='text-center'>
+            <h1 className='text-4xl text-gray-800 font-sans font-bold font-mono'>Try a quick search for</h1>
+            <h1 className='text-4xl text-gray-800 font-sans font-bold my-2 font-mono'>San Francisco</h1>
+          </div>
+
+          <div className='grid md:grid-cols-4 gap-4 px-2 items-center mt-12 text-gray-800'>
+            <div className='flex flex-col justify-center items-center border border-gray-300 py-8 rounded-xl shadow-xl  hover:text-white hover:bg-slate-900 '>
+              <FaBath className='text-blue-500' size={30}/>
+              <p className='font-bold font-mono text-xl mt-8'>Bathroom Remodel Contractors</p>
+            </div>
+            <div className='flex flex-col justify-center items-center border border-gray-300 py-8 rounded-xl shadow-xl  hover:text-white hover:bg-slate-900 '>
+              <MdOutlineShower className='text-pink-500 ' size={30}/>
+              <p className='font-bold font-mono text-xl mt-8'>Roofers</p>
+            </div>
+            <div className='flex flex-col justify-center items-center border border-gray-300 py-8 rounded-xl shadow-xl  hover:text-white hover:bg-slate-900 '>
+              <MdOutlineArchitecture className='text-green-500' size={30}/>
+              <p className='font-bold font-mono text-xl mt-8'>Remodeling Architects</p>
+            </div>
+            <div className='flex flex-col justify-center items-center border border-gray-300 py-8 rounded-xl shadow-xl hover:text-white hover:bg-slate-900 '>
+              <MdEngineering className='text-green-500' size={30}/>
+              <p className='font-bold font-mono text-xl mt-8'>Civil & Structural Engineers</p>
+            </div>
+
+            
+          </div>
+
+
+
+          <div className='mt-[200px] mb-8 '>
+            <h6 className='font-mono text-lg font-bold text-yellow-500 text-center'>Queck Search</h6>
+          </div>
+          <div className='text-center'>
+            <h1 className='text-4xl text-gray-800 font-sans font-bold font-mono'>Try a quick search for</h1>
+            <h1 className='text-4xl text-gray-800 font-sans font-bold my-2 font-mono'>San Francisco</h1>
+          </div>
+
+          <div className='grid md:grid-cols-4 gap-4 px-2 items-center mt-12 text-gray-800'>
+            <div className='flex flex-col justify-center items-center border border-gray-300 py-8 rounded-xl shadow-xl  hover:text-white hover:bg-slate-900 '>
+              <FaBath className='text-blue-500' size={30}/>
+              <p className='font-bold font-mono text-xl mt-8'>Bathroom Remodel Contractors</p>
+            </div>
+            <div className='flex flex-col justify-center items-center border border-gray-300 py-8 rounded-xl shadow-xl  hover:text-white hover:bg-slate-900 '>
+              <MdOutlineShower className='text-pink-500 ' size={30}/>
+              <p className='font-bold font-mono text-xl mt-8'>Roofers</p>
+            </div>
+            <div className='flex flex-col justify-center items-center border border-gray-300 py-8 rounded-xl shadow-xl  hover:text-white hover:bg-slate-900 '>
+              <MdOutlineArchitecture className='text-green-500' size={30}/>
+              <p className='font-bold font-mono text-xl mt-8'>Remodeling Architects</p>
+            </div>
+            <div className='flex flex-col justify-center items-center border border-gray-300 py-8 rounded-xl shadow-xl hover:text-white hover:bg-slate-900 '>
+              <MdEngineering className='text-green-500' size={30}/>
+              <p className='font-bold font-mono text-xl mt-8'>Civil & Structural Engineers</p>
+            </div>
+
+            
+          </div>
+          
+
+        </div>
+    </>
   )
 }
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,9 +4,9 @@ import Img2 from '../assets/img/img2.png'
 
 const Hero = () => {
   return (
-    <div className="w-screen h-screen bg-slate-900 flex flex-col justify-between text-white">
-      <div className="grid md:grid-cols-2 max-w-[1240px] m-auto">
-        <div className="flex flex-col kjustify-center md:items-start w-full px-2 py-8">
+    <div className="w-full h-[1240px] bg-slate-900 flex flex-col justify-between text-white">
+      <div className="grid md:grid-cols-2 max-w-[1240px] m-auto gap-[8rem]">
+        <div className="flex flex-col justify-center md:items-start w-full px-2 py-8">
           <p className="text-5xl md:text-6xl font-bold">
             Find Construction
           </p>
@@ -16,11 +16,11 @@ const Hero = () => {
           <button className="py-3 px-4 mt-4">Get Started</button>
         </div>
 
-        <div className="hidden  md:flex gap-1 relative">
-          <div className="block m-0 p-0">
-            <img className="h-[350px] w-[250px] object-cover rounded-tr-2xl absolute bottom-0 left-0 border-slate-900 border-[8px] block" src={Img1} alt="/" />
+        <div className="hidden  md:flex  gap-1 relative ">
+          <div className="rounded-tr-2xl absolute bottom-0 left-0 border-slate-900 border-[8px] block">
+            <img className="h-[350px] w-[250px] object-cover rounded-lg" src={Img1} alt="/" />
           </div>
-          <div>
+          <div className="ml-8">
             <img className="h-[650px] w-[650px] object-cover rounded-2xl " src={Img2} alt="/" />
           </div>
         </div>
